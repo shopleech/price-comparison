80b68fd5c467fcc744e7898fa58d4cd1f80431d2
Ahto Jalak <ahto.jalak@gmail.com>
2022-10-28 14:53:41 +0300

    update working stack

--- cdk/src/main/java/shopleech/CdkStack.java
+++ cdk/src/main/java/shopleech/CdkStack.java
@@ -18,7 +18,6 @@ import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.route53.*;
-import software.amazon.awscdk.services.route53.targets.ApiGateway;
 import software.amazon.awscdk.services.route53.targets.CloudFrontTarget;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.deployment.BucketDeployment;
@@ -26,10 +25,7 @@ import software.amazon.awscdk.services.s3.deployment.ISource;
 import software.amazon.awscdk.services.s3.deployment.Source;
 import software.constructs.Construct;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CdkStack extends Stack {
     public CdkStack(final Construct parent, final String id) {
@@ -68,7 +64,7 @@ public class CdkStack extends Stack {
         Function productApiFunction = new Function(this, "productApiFunction",
                 FunctionProps.builder()
                         .functionName("sl-product-api")
-                        .code(Code.fromAsset("./assets/product-api-0.1.0.jar"))
+                        .code(Code.fromAsset("../product-api/build/libs/product-api-0.1.0.jar"))
                         .handler("hello.handler")
                         .runtime(Runtime.JAVA_11)
                         .environment(lambdaEnvMap)
@@ -79,39 +75,22 @@ public class CdkStack extends Stack {
 
         dynamodbTable.grantReadWriteData(productApiFunction);
 
-        // api gateway cert
-        final ICertificate cert =
-                DnsValidatedCertificate.Builder.create(this, "ApiCertificate")
-                        .domainName("gateway.shopleech.com")
-                        .hostedZone(zone)
-                        .build();
-
         // defines a new api gateway for product-api
-        RestApi api = new RestApi(this, "productApi",
-                RestApiProps.builder().restApiName("Shopleech Product API")
-                        .domainName(DomainNameOptions.builder()
-                                .domainName("gateway.shopleech.com")
-                                .certificate(cert)
-                                .securityPolicy(SecurityPolicy.TLS_1_2)
-                                .build())
+        RestApi api = new RestApi(this, "ProductApi",
+                RestApiProps.builder()
+                        .restApiName("Shopleech Product API")
+                        .deploy(true)
                         .build());
 
         // set endpoint
-        IResource items = api.getRoot().addResource("products");
+        IResource items = api.getRoot().addResource("api").addResource("products");;
         Integration getAllIntegration = new LambdaIntegration(productApiFunction);
         items.addMethod("GET", getAllIntegration);
 
         // set endpoint
-        IResource singleItem = items.addResource("{id}");
+        IResource singleItem = items.addResource("test").addResource("{id}");
         Integration getOneIntegration = new LambdaIntegration(productApiFunction);
         singleItem.addMethod("GET", getOneIntegration);
-
-        // api alias record
-        ARecord.Builder.create(this, "ApiAliasRecord")
-                .recordName("gateway.shopleech.com")
-                .target(RecordTarget.fromAlias(new ApiGateway(api)))
-                .zone(zone)
-                .build();
 
         // website
         List<String> siteDomainList = new ArrayList<>(1);
@@ -144,37 +123,25 @@ public class CdkStack extends Stack {
         siteBucket.grantRead(originAccessIdentity);
 
         // cloudfront behaviours
-        List<String> forwardedHeaders = new ArrayList<>(1);
-        forwardedHeaders.add("Accept-Encoding");
-        forwardedHeaders.add("Authorization");
-        forwardedHeaders.add("Accept");
-        forwardedHeaders.add("X-Origin-Verify");
-        forwardedHeaders.add("Host");
-
-        List<Behavior> behavioursList = new ArrayList<>(1);
-        behavioursList.add(Behavior.builder().isDefaultBehavior(true).compress(true).build());
+        List<Behavior> s3BehavioursList = new ArrayList<>(1);
+        s3BehavioursList.add(
+                Behavior.builder()
+                        .isDefaultBehavior(true).build());
 
         List<Behavior> apiBehavioursList = new ArrayList<>(1);
         apiBehavioursList.add(
                 Behavior.builder()
                         .pathPattern("/api/*")
                         .allowedMethods(CloudFrontAllowedMethods.ALL)
-                        .forwardedValues(CfnDistribution.ForwardedValuesProperty.builder()
-                                .queryString(true)
-                                .headers(forwardedHeaders).build())
-                        .compress(true)
+//                        .forwardedValues(
+//                                CfnDistribution.ForwardedValuesProperty.builder()
+//                                        .queryString(true)
+//                                        .headers(Arrays.asList("Authorization"))
+//                                        .build())
                         .build());
 
         // source
         List<SourceConfiguration> sourceConfigurationsList = new ArrayList<>(1);
-        sourceConfigurationsList.add(
-                SourceConfiguration.builder()
-                        .customOriginSource(
-                                CustomOriginConfig.builder().domainName("gateway.shopleech.com")
-                                        .originPath("/prod")
-                                        .build())
-                        .behaviors(apiBehavioursList)
-                        .build());
         sourceConfigurationsList.add(
                 SourceConfiguration.builder()
                         .s3OriginSource(
@@ -182,7 +149,17 @@ public class CdkStack extends Stack {
                                         .s3BucketSource(siteBucket)
                                         .originAccessIdentity(originAccessIdentity)
                                         .build())
-                        .behaviors(behavioursList)
+                        .behaviors(s3BehavioursList)
+                        .build());
+        sourceConfigurationsList.add(
+                SourceConfiguration.builder()
+                        .customOriginSource(
+                                CustomOriginConfig.builder()
+                                        .domainName(String.format("%s.execute-api.eu-west-1.amazonaws.com",
+                                                api.getRestApiId()))
+                                        .originPath(String.format("/%s", api.getDeploymentStage().getStageName()))
+                                        .build())
+                        .behaviors(apiBehavioursList)
                         .build());
 
         CloudFrontWebDistribution distribution =
@@ -205,7 +182,7 @@ public class CdkStack extends Stack {
 
         // Deploy site contents to S3 bucket
         List<ISource> sources = new ArrayList<>(1);
-        sources.add(Source.asset("./site-contents"));
+        sources.add(Source.asset("../web-app/dist"));
 
         BucketDeployment.Builder.create(this, "DeployWithInvalidation")
                 .sources(sources)
@@ -213,9 +190,14 @@ public class CdkStack extends Stack {
                 .distribution(distribution)
                 .build();
 
-        CfnOutput.Builder.create(this, "DistributionId")
+        CfnOutput.Builder.create(this, "CloudFrontDistributionId")
                 .description("CloudFront Distribution Id")
                 .value(distribution.getDistributionId())
+                .build();
+
+        CfnOutput.Builder.create(this, "CloudfrontDistributionURL")
+                .description("CloudFront Distribution URL")
+                .value(String.format("https://%s", distribution.getDistributionDomainName()))
                 .build();
     }
 }
