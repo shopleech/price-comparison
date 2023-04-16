5758f281c5cdce064e3ab316a18a269478ed4217
Ahto Jalak <ahto.jalak@gmail.com>
2023-04-16 12:02:05 +0300

    fix prod base url

--- price-comparison-client1/src/http-client.ts
+++ price-comparison-client1/src/http-client.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
  * @since 06.02.2023
  */
 export const httpCLient = axios.create({
-    baseURL: (process.env.NODE_ENV === 'production') ? 'https://api.shopleech.com:8080' : 'http://localhost:8080',
+    baseURL: (process.env.NODE_ENV === 'production') ? 'https://api.shopleech.com' : 'http://localhost:8080',
     headers: {
         'Content-type': 'application/json'
     },
