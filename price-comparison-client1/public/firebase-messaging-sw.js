bfc699d2a10b5cb6e2188752a203ba9f635bc7ec
Ahto Jalak <ahto.jalak@gmail.com>
2023-06-03 17:40:53 +0300

    fix map

--- price-comparison-client1/public/firebase-messaging-sw.js
+++ price-comparison-client1/public/firebase-messaging-sw.js
@@ -1,10 +1,13 @@
-import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
-import { initializeApp } from "firebase/app";
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyCWPyQ_47Nqn7hsLsNzlR21C6lbB7yr3RQ",
     authDomain: "price-comparison-vue.firebaseapp.com",
     projectId: "price-comparison-vue",
@@ -16,9 +19,13 @@ const firebaseApp = initializeApp({
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = getMessaging(firebaseApp);
-onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+        '[firebase-messaging-sw.js] Received background message ',
+        payload
+    );
     // Customize notification here
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
@@ -26,6 +33,5 @@ onBackgroundMessage(messaging, (payload) => {
         icon: '/bell.png'
     };
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
