5394369607e3bcec734b22ca983a3595b83a46d4
Ahto Jalak <ahto.jalak@gmail.com>
2024-03-17 17:44:09 +0200

    fix frontend github actions workflow; add missing components for google auth

---
+++ price-comparison-client1/src/components/GoogleAuthButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+const login = () => {
+    console.log("klikk");
+}
+</script>
+
+<template>
+  <button @click="login">Login Using Google</button>
+</template>
---
+++ price-comparison-client1/src/components/GoogleLogin.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { googleSdkLoaded } from "vue3-google-login"
+const login = () => {
+  googleSdkLoaded((google) => {
+    google.accounts.oauth2.initCodeClient({
+      client_id: '431621825092-e2pah2n24bubb2qk5asti0p5dnc66t7l.apps.googleusercontent.com',
+      scope: 'email profile openid',
+      callback: (response) => {
+        console.log("Handle the response", response)
+      }
+    }).requestCode()
+  })
+}
+</script>
+
+<template>
+  <button @click="login">Login Using Google</button>
+</template>
